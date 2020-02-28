4fd252f88e56fe12062724c84e018d60ca2865c1
JoeCornish <g6doug@gmail.com>
2020-02-28 11:32:38 +0000

    deleted

--- script.js
+++
@@ -1,5 +0,0 @@
-//Copy value to a1;
-document.getElementById('a1').value = answer1;
-document.getElementById('a2').value = answer2;
-document.getElementById('a3').value = answer3;
-document.getElementById('a4').value = answer4;
